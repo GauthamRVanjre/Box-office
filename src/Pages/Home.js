7c7c711fa0bd2ad97067aeeb3d31ae746e731e46
GauthamRVanjre <vanjregautham@gmail.com>
2022-11-11 21:13:38 +0530

    rendered results on screen

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import MainPageLayout from '../components/MainPageLayout'
 const Home = () => {
 
   const [inputText,setInputText] = useState('');
+  const [results,setResults] = useState(null);
 
   const onInputChange = e => {
     
@@ -14,7 +15,9 @@ const Home = () => {
     //https://api.tvmaze.com/search/shows?q=girls
     fetch(`https://api.tvmaze.com/search/shows?q=${inputText}`)
     .then(r => r.json())
-    .then(response => console.log(response))
+    .then(response =>
+      setResults(response)
+      )
   }
 
   const onKeyDown = (e) => {
@@ -23,10 +26,26 @@ const Home = () => {
     }
   }
 
+  const renderResults = () => {
+    if(results && results.length === 0){
+      return <div>No results found</div>
+    }
+    if(results && results.length > 0){
+      return <div>{
+          results.map((result) => (
+            <div key={result.show.id}>{result.show.name}</div>
+          ))
+        }</div>
+    }
+
+    return null;
+  }
+
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={inputText}/>
       <button type="button" onClick={onSearch}>Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
