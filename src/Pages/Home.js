6cf78e9ac858658c4ea1a719b52af87f82a3dab7
GauthamRVanjre <vanjregautham@gmail.com>
2022-11-11 20:49:47 +0530

    completed fetching results from API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+
+  const [inputText,setInputText] = useState('');
+
+  const onInputChange = e => {
+    
+    setInputText(e.target.value);
+  }
+
+  const onSearch = () =>{
+    //https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${inputText}`)
+    .then(r => r.json())
+    .then(response => console.log(response))
+  }
+
+  const onKeyDown = (e) => {
+    if(e.keyCode === 13){
+      onSearch();
+    }
+  }
+
   return (
-    <MainPageLayout>This is Home</MainPageLayout>
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={inputText}/>
+      <button type="button" onClick={onSearch}>Search</button>
+    </MainPageLayout>
   )
 }
 
