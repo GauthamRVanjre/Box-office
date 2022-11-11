4dd9ea8373f07b77cd6e6cc727dd4a29d9c8b4a1
GauthamRVanjre <vanjregautham@gmail.com>
2022-11-11 19:55:44 +0530

    completed router part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
   return (
     <div>
-      <h1>Welocme to react</h1>
+      <Navs/>
+      <Switch>
+        <Route exact path="/"> 
+          <Home/>
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred/>
+        </Route>
+        <Route> This is 404 page</Route>
+      </Switch>
     </div>
   );
 }
